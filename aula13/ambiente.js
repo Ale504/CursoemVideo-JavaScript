07e028752d09ebf33365073812adeb39d9b6e186
Ale504 <ale_alpha_504@hotmail.com>
2021-03-03 09:20:22 -0300

    Curso em video

---
+++ aula13/ambiente.js
@@ -0,0 +1,31 @@
+var c = 1
+do{
+  console.log(`passo ${c}`)
+  c++
+} while (c <= 50)
+ 
+  
+
+ 
+ /*
+ var c = 1
+ while (c <= 10) {
+   console.log(`passo ${c}`)
+   //console.log('Tudo Bem ?')
+   c++
+ }
+ */
+ 
+ 
+ 
+ 
+ 
+ 
+ /*
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+*/
